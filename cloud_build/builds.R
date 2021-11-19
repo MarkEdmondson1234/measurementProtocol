4db6275bdd21c382d378326ba12a8e4649f48d38
Mark Edmondson <github@markedmondson.me>
2021-11-19 12:01:02 +0100

    build for gcer_public

--- cloud_build/builds.R
+++ cloud_build/builds.R
@@ -1,6 +1,7 @@
 library(googleCloudRunner)
 
 repo <- "MarkEdmondson1234/measurementProtocol"
+
 cr_deploy_pkgdown(repo,
                   secret = "github-ssh",
                   cloudbuild_file = "cloud_build/pkgdown.yml")
@@ -13,5 +14,5 @@ cr_deploy_packagetests(
 cr_deploy_docker_trigger(
   repo = cr_buildtrigger_repo(repo),
   image = "measurementProtocol",
-  projectId = "gcer-public"
+  projectId_target = "gcer-public"
 )
