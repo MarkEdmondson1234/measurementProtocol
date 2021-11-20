7755f819b68ed1c725bdbf31c8ef8da059b4af88
Mark Edmondson <github@markedmondson.me>
2021-11-20 17:26:29 +0100

    setup the plumber trigger

--- cloud_build/builds.R
+++ cloud_build/builds.R
@@ -19,8 +19,18 @@ cr_deploy_docker_trigger(
   timeout = 3600
 )
 
+cr_deploy_docker_trigger(
+  repo = cr_buildtrigger_repo(repo),
+  image = "measurement-protocol-proxy",
+  dir = "inst/plumber",
+  includedFiles = "inst/plumber/**",
+  timeout = 3600
+)
+
 cr_deploy_plumber(
   "inst/plumber",
   remote = "measurement_protocol_proxy",
   env_vars = paste0("MP_SECRET=", Sys.getenv("MP_SECRET"))
 )
+
+# curl -X POST "https://measurement-protocol-proxy-ewjogewawq-ew.a.run.app/gtm?gtm_id=dfdsfsf" -H "accept: application/json" -d '{"event_name":"hi","client_id":"1234"}'
